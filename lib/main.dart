831aadfed847a21368c05dfd1b174a249c5f63a2
khumoyiddin <khumoyiddin.bakhodirov@mymo.com>
2022-06-07 23:04:50 +0500

    merging conflicts resolved

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,3 @@ import 'package:endless_photo_scroll/ui/app_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const AppWidget());
-void main() => runApp(const EndlessPhotoScrollApp());
-
-class EndlessPhotoScrollApp extends StatelessWidget {
-  const EndlessPhotoScrollApp({Key? key}) : super(key: key);
-
-  void getNothing() {}
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: FirstScreen(),
-    );
-  }
-}
